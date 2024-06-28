a0568eb8571737836a570ad8df3b994581da67e5
ShivGitAnsh <169175039+ShivGitAnsh@users.noreply.github.com>
2024-06-29 01:06:25 +0530

    Update AppointmentForm.jsx

--- Frontend/src/components/AppointmentForm.jsx
+++ Frontend/src/components/AppointmentForm.jsx
@@ -34,7 +34,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "https://hospital-umber.vercel.app/api/v1/user/doctors",
+        "https://hospital-management-peach.vercel.app//api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
