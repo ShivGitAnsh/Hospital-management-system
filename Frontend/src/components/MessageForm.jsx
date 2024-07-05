c9d2ad44d03cd3cd7c0937487278f6d3fcbc2098
ShivGitAnsh <169175039+ShivGitAnsh@users.noreply.github.com>
2024-07-05 16:03:56 +0530

    Update MessageForm.jsx

--- Frontend/src/components/MessageForm.jsx
+++ Frontend/src/components/MessageForm.jsx
@@ -14,7 +14,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          "https://hospital-management-peach.vercel.app/api/v1/message/send",
+          "https://hospital-management-backend-kuml.onrender.com/api/v1/message/send",
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
