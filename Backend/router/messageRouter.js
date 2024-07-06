8bda874554d839ba48283e39c87246576773233d
ShivGitAnsh <169175039+ShivGitAnsh@users.noreply.github.com>
2024-07-06 23:48:16 +0530

    Update messageRouter.js

--- Backend/router/messageRouter.js
+++ Backend/router/messageRouter.js
@@ -7,6 +7,6 @@ import { isAdminAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/send", sendMessage);
-router.get("/getall", getAllMessages);
+router.get("/getall" , isAdminAuthenticated, getAllMessages);
 
 export default router;
