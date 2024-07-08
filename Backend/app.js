e5785a00933639f86bffda3db178644f098ce36f
ShivGitAnsh <169175039+ShivGitAnsh@users.noreply.github.com>
2024-07-08 23:24:31 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ console.log("FRONTEND_URL_ONE:", process.env.FRONTEND_URL_ONE);
 console.log("FRONTEND_URL_TWO:", process.env.FRONTEND_URL_TWO);
 
 app.use(cors({
-    origin: ["https://hospital-management-system-3ddf.vercel.app",process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    origin: ["https://hospital-management-system-3ddf.vercel.app","https://hospital-management-dashboard-omega.vercel.app/login",process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,})
     );
