d6037255e36865cee61c2adbcf2f404874237b10
ShivGitAnsh <169175039+ShivGitAnsh@users.noreply.github.com>
2024-07-09 00:47:57 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 
 export const Context = createContext({
   isAuthenticated: false,
+  setIsAuthenticated:()=>{},
+  user:{},
+  setUser:()=>{}
 });
 
 const AppWrapper = () => {
